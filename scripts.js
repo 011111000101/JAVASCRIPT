02ca66b56236409e162250aafe8fdc46bea8eb79
011111000101 <87422590+011111000101@users.noreply.github.com>
2022-02-09 16:27:04 +0100

    1.0

---
+++ scripts.js
@@ -0,0 +1,31 @@
+console.log("Break until 17:10")
+console.log("This is JavaScript");
+let colors = ["orange", "blue", "green", "black", "white", "red"];
+let post = {
+     title : "My post",
+     description : "My firs post, awesome!",
+     likeCount : 50,
+     comments : ["cool!", "nice job!"]
+};
+
+console.log(colors);
+console.log(post);
+
+let age = 50;
+
+if (post.likeCount == 50) {
+    console.log("Hovno");
+} else {
+    console.log("prdel");
+};
+
+console.log("Vypíše se " + colors.length + " barev.");
+
+for (let i = 0; i < colors.length; i++) {
+console.log(colors[i]);    
+};
+
+
+
+
+console.log("This is the end of the web")
